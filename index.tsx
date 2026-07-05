55fa39a69bcdf7b5ab95ba5bd216d3b75eaf4b6d
bekzhanduisengali <beckzhan04@gmail.com>
2026-07-05 16:43:01 +0500

    fix

--- index.tsx
+++ index.tsx
@@ -3,6 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const setViewportHeightUnit = () => {
+  document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+};
+
+setViewportHeightUnit();
+window.addEventListener('resize', setViewportHeightUnit);
+window.addEventListener('orientationchange', setViewportHeightUnit);
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
